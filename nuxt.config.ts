f8184c8aec10f8750eed91acaf97078e04a23566
iqmotion3 <iqmotion3@192.168.1.74>
2022-04-21 11:39:42 +0300

    chore: bump dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  builder: 'webpack',
   css: ['~/assets/css/tailwind.css', '~/assets/css/fonts.css'],
   build: {
     postcss: {
