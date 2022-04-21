4909ea9a0363cd01d219efbcf743efe4e34192fb
iqmotion3 <iqmotion3@192.168.1.74>
2022-04-21 16:42:01 +0300

    fix: cleaning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  builder: 'webpack',
   css: ['~/assets/css/tailwind.css', '~/assets/css/fonts.css'],
   build: {
     postcss: {
